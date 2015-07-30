d16e075a27ab33ba9ad9229b611351908811b777
pemeoluoj <pemeoluoj@KATRINA-VAIO>
2015-07-30 15:18:56 +0700

    thong bao

--- module/Application/src/Application/Models/chatModel.php
+++ module/Application/src/Application/Models/chatModel.php
@@ -4,6 +4,7 @@ namespace Application\Models;
 use Application\base\baseModel;
 use Application\Entity\ketqua;
 use Application\Entity\user;
+use Application\Unlity\Unlity;
 
 class chatModel extends baseModel
 {
@@ -18,7 +19,7 @@ class chatModel extends baseModel
         
         try {
         
-            $obj = $qb = $this->em->createQueryBuilder()->
+            $obj = $this->em->createQueryBuilder()->
             select(array('chat', 'user'))
             ->from('Application\Entity\chat', 'chat')
             ->join('chat.user', 'user')
@@ -46,6 +47,327 @@ class chatModel extends baseModel
             return $kq;
         }
     }
-
+    
+    /**
+     * 
+     * @param user $user
+     * @return \Application\Entity\ketqua  */
+    public function DemTamNghi($user)
+    {
+        $kq = new ketqua();
+    
+        try {
+            $today = (new \DateTime())->format('Y-m-d');
+            
+            $obj = $this->em->createQueryBuilder()->
+            select('COUNT(n)')
+            ->from('Application\Entity\nguoinopthue', 'n')
+            ->join('n.thongtinngungnghis', 't')
+            ->join('n.usernnts','usernnts')
+            ->join('usernnts.user', 'user')
+            ->where('usernnts.ThoiGianKetThuc is null')
+            ->andWhere('user.coquanthue = ?2')
+            ->andWhere('t.TuNgay <= ?1')
+            ->andWhere('t.DenNgay >= ?1')
+            ->setParameter(1,$today)
+            ->setParameter(2, $user->getCoquanthue())
+            ->getQuery()
+            ->getOneOrNullResult();
+            if($obj!=NULL){
+                $kq->setObj($obj);
+                $kq->setKq(true);
+                $kq->setMessenger("Đếm thành công !");
+            }
+            else{
+                $kq->setObj($obj);
+                $kq->setKq(false);
+            }
+    
+            return $kq;
+        } catch (\Exception $e) {
+            $kq->setKq(false);
+            $kq->setMessenger($e->getMessage());
+            return $kq;
+        }
+    }
+    
+    public function DemChuaDK($user)
+    {
+        $kq = new ketqua();
+    
+        try {  
+            $DQL = $this->em->createQueryBuilder()
+            ->select('n1.MaSoThue')
+            ->from('Application\Entity\nguoinopthue', 'n1')
+            ->innerJoin('n1.dukienmbs', 'd')
+            ->getDQL();
+            
+            
+            $obj = $this->em->createQueryBuilder()->
+            select('COUNT(n)')
+            ->from('Application\Entity\nguoinopthue', 'n')
+            ->join('n.usernnts','usernnts')
+            ->join('usernnts.user', 'user')
+            ->where('usernnts.ThoiGianKetThuc is null')
+            ->andWhere('user.coquanthue = ?1')
+            ->andWhere("n.MaSoThue NOT IN ($DQL)")
+            ->setParameter(1, $user->getCoquanthue())
+            ->getQuery()
+            ->getOneOrNullResult();
+            if($obj!=NULL){
+                $kq->setObj($obj);
+                $kq->setKq(true);
+                $kq->setMessenger("Đếm thành công !");
+            }
+            else{
+                $kq->setObj($obj);
+                $kq->setKq(false);
+            }
+    
+            return $kq;
+        } catch (\Exception $e) {
+    
+            $kq->setKq(false);
+            $kq->setMessenger($e->getMessage());
+            return $kq;
+        }
+    }
+    
+    public function ThueMB($user)
+    {
+        $kq = new ketqua();
+    
+        try {    
+    
+            $obj = $this->em->createQueryBuilder()->
+            select('COUNT(n)')
+            ->from('Application\Entity\thuemonbai', 'thuemonbai')
+            ->join('thuemonbai.nguoinopthue', 'n')
+            ->join('n.usernnts','usernnts')
+            ->join('usernnts.user', 'user')
+            ->where('usernnts.ThoiGianKetThuc is null')
+            ->andWhere('user.coquanthue = ?1')
+            ->andWhere('thuemonbai.TrangThai = ?2')
+            ->setParameter(1, $user->getCoquanthue())
+            ->setParameter(2, 0)
+            ->getQuery()
+            ->getOneOrNullResult();
+            if($obj!=NULL){
+                $kq->setObj($obj);
+                $kq->setKq(true);
+                $kq->setMessenger("Đếm thành công !");
+            }
+            else{
+                $kq->setObj($obj);
+                $kq->setKq(false);
+            }
+    
+            return $kq;
+        } catch (\Exception $e) {
+    
+            $kq->setKq(false);
+            $kq->setMessenger($e->getMessage());
+            return $kq;
+        }
+    }
+    
+    public function ThueKhoan($user)
+    {
+        $kq = new ketqua();
+    
+        try {
+    
+            $obj = $this->em->createQueryBuilder()->
+            select('COUNT(n)')
+            ->from('Application\Entity\thue', 'thue')
+            ->join('thue.nguoinopthue', 'n')
+            ->join('n.usernnts','usernnts')
+            ->join('usernnts.user', 'user')
+            ->join('thue.muclucngansach', 'muclucngansach')
+            ->where('usernnts.ThoiGianKetThuc is null')
+            ->andWhere('user.coquanthue = ?1')
+            ->andWhere('thue.TrangThai = ?2')
+            ->andWhere("muclucngansach.TieuMuc like '1701' OR muclucngansach.TieuMuc like '1003'")
+            ->setParameter(1, $user->getCoquanthue())
+            ->setParameter(2, 0)
+            ->getQuery()
+            ->getOneOrNullResult();
+            if($obj!=NULL){
+                $kq->setObj($obj);
+                $kq->setKq(true);
+                $kq->setMessenger("Đếm thành công !");
+            }
+            else{
+                $kq->setObj($obj);
+                $kq->setKq(false);
+            }
+    
+            return $kq;
+        } catch (\Exception $e) {
+    
+            $kq->setKq(false);
+            $kq->setMessenger($e->getMessage());
+            return $kq;
+        }
+    }
+    
+    public function ThueTTDB($user)
+    {
+        $kq = new ketqua();
+    
+        try {
+    
+            $obj = $this->em->createQueryBuilder()->
+            select('COUNT(n)')
+            ->from('Application\Entity\thue', 'thue')
+            ->join('thue.nguoinopthue', 'n')
+            ->join('n.usernnts','usernnts')
+            ->join('usernnts.user', 'user')
+            ->join('thue.muclucngansach', 'muclucngansach')
+            ->where('usernnts.ThoiGianKetThuc is null')
+            ->andWhere('user.coquanthue = ?1')
+            ->andWhere('thue.TrangThai = ?2')
+            ->andWhere("muclucngansach.TieuMuc like '1757'")
+            ->setParameter(1, $user->getCoquanthue())
+            ->setParameter(2, 0)
+            ->getQuery()
+            ->getOneOrNullResult();
+            if($obj!=NULL){
+                $kq->setObj($obj);
+                $kq->setKq(true);
+                $kq->setMessenger("Đếm thành công !");
+            }
+            else{
+                $kq->setObj($obj);
+                $kq->setKq(false);
+            }
+    
+            return $kq;
+        } catch (\Exception $e) {
+    
+            $kq->setKq(false);
+            $kq->setMessenger($e->getMessage());
+            return $kq;
+        }
+    }
+    
+    public function ThueTN($user)
+    {
+        $kq = new ketqua();
+    
+        try {
+    
+            $obj = $this->em->createQueryBuilder()->
+            select('COUNT(n)')
+            ->from('Application\Entity\thue', 'thue')
+            ->join('thue.nguoinopthue', 'n')
+            ->join('n.usernnts','usernnts')
+            ->join('usernnts.user', 'user')
+            ->join('thue.muclucngansach', 'muclucngansach')
+            ->where('usernnts.ThoiGianKetThuc is null')
+            ->andWhere('user.coquanthue = ?1')
+            ->andWhere('thue.TrangThai = ?2')
+            ->andWhere("muclucngansach.TieuMuc like '3801'")
+            ->setParameter(1, $user->getCoquanthue())
+            ->setParameter(2, 0)
+            ->getQuery()
+            ->getOneOrNullResult();
+            if($obj!=NULL){
+                $kq->setObj($obj);
+                $kq->setKq(true);
+                $kq->setMessenger("Đếm thành công !");
+            }
+            else{
+                $kq->setObj($obj);
+                $kq->setKq(false);
+            }
+    
+            return $kq;
+        } catch (\Exception $e) {
+    
+            $kq->setKq(false);
+            $kq->setMessenger($e->getMessage());
+            return $kq;
+        }
+    }
+    
+    public function ThueBVMT($user)
+    {
+        $kq = new ketqua();
+    
+        try {
+    
+            $obj = $this->em->createQueryBuilder()->
+            select('COUNT(n)')
+            ->from('Application\Entity\thue', 'thue')
+            ->join('thue.nguoinopthue', 'n')
+            ->join('n.usernnts','usernnts')
+            ->join('usernnts.user', 'user')
+            ->join('thue.muclucngansach', 'muclucngansach')
+            ->where('usernnts.ThoiGianKetThuc is null')
+            ->andWhere('user.coquanthue = ?1')
+            ->andWhere('thue.TrangThai = ?2')
+            ->andWhere("muclucngansach.TieuMuc like '2601'")
+            ->setParameter(1, $user->getCoquanthue())
+            ->setParameter(2, 0)
+            ->getQuery()
+            ->getOneOrNullResult();
+            if($obj!=NULL){
+                $kq->setObj($obj);
+                $kq->setKq(true);
+                $kq->setMessenger("Đếm thành công !");
+            }
+            else{
+                $kq->setObj($obj);
+                $kq->setKq(false);
+            }
+    
+            return $kq;
+        } catch (\Exception $e) {
+    
+            $kq->setKq(false);
+            $kq->setMessenger($e->getMessage());
+            return $kq;
+        }
+    }
+    
+    public function DKThue($user)
+    {
+        $kq = new ketqua();
+    
+        try {
+    
+            $obj = $this->em->createQueryBuilder()->
+            select('COUNT(n)')
+            ->from('Application\Entity\dukienthue', 'dukienthue')
+            ->join('dukienthue.nguoinopthue', 'n')
+            ->join('n.usernnts','usernnts')
+            ->join('usernnts.user', 'user')
+            ->where('usernnts.ThoiGianKetThuc is null')
+            ->andWhere('user.coquanthue = ?1')
+            ->andWhere('dukienthue.TrangThai = ?2')
+            ->andWhere("dukienthue.KyThue not like '%/%'")
+            ->setParameter(1, $user->getCoquanthue())
+            ->setParameter(2, 0)
+            ->getQuery()
+            ->getOneOrNullResult();
+            if($obj!=NULL){
+                $kq->setObj($obj);
+                $kq->setKq(true);
+                $kq->setMessenger("Đếm thành công !");
+            }
+            else{
+                $kq->setObj($obj);
+                $kq->setKq(false);
+            }
+    
+            return $kq;
+        } catch (\Exception $e) {
+    
+            $kq->setKq(false);
+            $kq->setMessenger($e->getMessage());
+            return $kq;
+        }
+    }
 }
 
--- module/Quanlyuser/src/Quanlyuser/Controller/menuController.php
+++ module/Quanlyuser/src/Quanlyuser/Controller/menuController.php
@@ -21,8 +21,42 @@ class menuController extends baseController
         $model = new chatModel($this->getEntityManager());
         
         $dsChat = $model->DanhSachChat($this->getUser());
+        
+        //thongbao
+        $temp = $model->DemTamNghi($this->getUser());
+        $demtn = ($temp->getKq()==true)?$temp->getObj():0;
+        
+        $temp = $model->DemChuaDK($this->getUser());
+        $demchuadk = ($temp->getKq()==true)?$temp->getObj():0;
+        
+        $temp = $model->ThueMB($this->getUser());
+        $thuemb = ($temp->getKq()==true)?$temp->getObj():0;
+        
+        $temp = $model->ThueKhoan($this->getUser());
+        $thuekhoan = ($temp->getKq()==true)?$temp->getObj():0;
+        
+        $temp = $model->ThueTTDB($this->getUser());
+        $thuettdb = ($temp->getKq()==true)?$temp->getObj():0;
+        
+        $temp = $model->ThueTN($this->getUser());
+        $thuetn = ($temp->getKq()==true)?$temp->getObj():0;
+        
+        $temp = $model->ThueBVMT($this->getUser());
+        $thuebvmt = ($temp->getKq()==true)?$temp->getObj():0;
+        
+        $temp = $model->DKThue($this->getUser());
+        $dkthue = ($temp->getKq()==true)?$temp->getObj():0;
+        
         return array(
-            'dsChat' => $dsChat->getObj()
+            'dsChat' => $dsChat->getObj(),
+            'demtn' => $demtn,
+            'demchuadk' => $demchuadk,
+            'thuemb' => $thuemb,
+            'thuekhoan' => $thuekhoan,
+            'thuettdb' => $thuettdb,
+            'thuetn' => $thuetn,
+            'thuebvmt' => $thuebvmt,
+            'dkthue' => $dkthue
         );
     }
 
